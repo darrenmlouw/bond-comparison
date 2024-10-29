4e7e3ff3654db72aca1484cab83aac7b72e3044f
Darren Louw <darrenmlouw@gmail.com>
2024-10-29 15:44:08 +0200

    Removed Location Checker

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,20 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@/contexts/ThemeContext.tsx';
 import { SalaryProvider } from '@/contexts/SalaryContext';
-import LocationChecker from '@/components/LocationChecker.tsx';
+// import LocationChecker from '@/components/LocationChecker.tsx';
 import { ComparisonProvider } from '@/contexts/ComparisonContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <LocationChecker>
+        {/* <LocationChecker> */}
           <SalaryProvider>
             <ComparisonProvider>
               <App />
             </ComparisonProvider>
           </SalaryProvider>
-        </LocationChecker>
+        {/* </LocationChecker> */}
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
