00c25ec2572d97e91c1b7d9c99252dcf843d3e56
Darren Louw <darrenmlouw@gmail.com>
2024-10-29 16:24:29 +0200

    Performance Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
 // src/App.tsx
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+
+import { Route, Routes } from 'react-router-dom';
 import Layout from '@/components/layout/Layout';
-import HomePage from '@/pages/Home/HomePage';
-import SalaryPage from '@/pages/Salary/SalaryPage';
-import ComparisonPage from '@/pages/Comparison/ComparisonPage';
-import { useEffect, useState } from 'react';
-import { WebsiteInitialScreen } from '@/components/WebsiteInitialScreen';
-import ScrollToTop from '@/components/ScrollToTop'; // Import the ScrollToTop component
+const HomePage = lazy(() => import('@/pages/Home/HomePage'));
+const SalaryPage = lazy(() => import('@/pages/Salary/SalaryPage'));
+const ComparisonPage = lazy(() => import('@/pages/Comparison/ComparisonPage'));
+import ScrollToTop from '@/components/ScrollToTop';
 
 function App() {
-  const location = useLocation();
-
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 900);
-  }, []);
-
   return (
-    <>
-      {loading ? (
-        <WebsiteInitialScreen />
-      ) : (
-        <>
-          <ScrollToTop />
-          <Routes location={location}>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route path="/Salary" element={<SalaryPage />} />
-              <Route path="/Comparison" element={<ComparisonPage />} />
-            </Route>
-          </Routes>
-        </>
-      )}
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ScrollToTop />
+      <Routes location={location}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/Salary" element={<SalaryPage />} />
+          <Route path="/Comparison" element={<ComparisonPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@/contexts/ThemeContext.tsx';
 import { SalaryProvider } from '@/contexts/SalaryContext';
-// import LocationChecker from '@/components/LocationChecker.tsx';
 import { ComparisonProvider } from '@/contexts/ComparisonContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        {/* <LocationChecker> */}
           <SalaryProvider>
             <ComparisonProvider>
               <App />
             </ComparisonProvider>
           </SalaryProvider>
-        {/* </LocationChecker> */}
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
