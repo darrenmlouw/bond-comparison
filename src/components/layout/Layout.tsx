f4739383080ba99551bb529ecdcec44b087c7ce8
Darren Louw <darrenmlouw@gmail.com>
2024-10-29 15:29:09 +0200

    Added Speed Insights

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,6 +5,7 @@ import TopBar from '@/components/layout/components/TopBar';
 import { Outlet } from 'react-router-dom';
 import { HomeIcon, Layers, CircleDollarSign } from 'lucide-react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const Layout = () => {
   const dockItems = [
@@ -24,6 +25,7 @@ const Layout = () => {
         <Outlet />
       </div>
       <Analytics />
+      <SpeedInsights />
       <div className="z-20 fixed bottom-12 sm:bottom-2 md:bottom-4 left-1/2 transform -translate-x-1/2 bg-primary/20 backdrop-blur-[6px] outline outline-1 outline-foreground/30 rounded-2xl">
         <FloatingDock
           items={dockItems}
@@ -31,6 +33,7 @@ const Layout = () => {
           mobileClassName="shadow-xl"
         />
       </div>
+      
     </>
   );
 };
