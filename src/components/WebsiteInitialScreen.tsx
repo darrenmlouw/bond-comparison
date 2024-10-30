63cafe998fcf2d51dedebc91662ef27c569013cf
Darren <darrenmlouw@gmail.com>
2024-10-30 10:48:10 +0200

    Preload Image

--- src/components/WebsiteInitialScreen.tsx
+++ src/components/WebsiteInitialScreen.tsx
@@ -5,7 +5,7 @@ export const WebsiteInitialScreen: React.FC = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-full h-full">
       <div className="flex items-center justify-center w-10 h-10 sm:w-15 sm:h-15 md:w-20 md:h-20 rounded-full animate-ping">
-        <img src={HouseLogo} alt="Logo" />
+      <img src={HouseLogo} alt="Logo" width="128" height="128" />
       </div>
     </div>
   );
