49b8936ab1851bd9a3aab51d27248f26bbefc426
Darren <darrenmlouw@gmail.com>
2024-11-20 16:57:47 +0200

    Nav Bar Changes

--- src/components/layout/components/FloatingDock.tsx
+++ src/components/layout/components/FloatingDock.tsx
@@ -49,7 +49,7 @@ const FloatingDockMobile = ({
   return (
     <div
       className={cn(
-        'mx-auto flex w-screen justify-around h-16  items-end rounded-t-xlmd:rounded-2xl px-8 pb-3 md:hidden',
+        'mx-auto flex w-screen justify-around h-20  items-start rounded-t-xlmd:rounded-2xl px-8 pt-3 md:hidden',
         className
       )}
     >
